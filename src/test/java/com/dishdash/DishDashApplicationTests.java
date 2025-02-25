e374fdf2f3edd6e394e4ad5fbbedbec5b365cb54
Joseph Slater <josephslater03@gmail.com>
2025-02-25 17:44:44 -0500

    Initial Commit

---
+++ src/test/java/com/dishdash/DishDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishDashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
